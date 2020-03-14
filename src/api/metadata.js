54196b9ab6bbf9ee3b936d68ef40f51c00374d5d
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2020-03-14 05:54:53 +0100

    output.

--- src/api/metadata.js
+++ src/api/metadata.js
@@ -3,16 +3,15 @@ import { getDb, getMetadata, addDatasetToDb } from '../database/index.js';
 
 export const add = async ( ctx ) => {
     log(`receiving metadata for hash = ${ctx.state.hash}`);
-    log(`received: ${ctx.state.meta}`);
     const db = getDb();
     await addDatasetToDb(db, ctx.state.hash, ctx.state.meta);
 };
 export const get = async (ctx) => {
     const hash = ctx.params.hash;
-    log(`getting metadata for hash = ${hash}`);
     if( ! await datasetExists(ctx, hash) ) {
         throw(new Error(`dataset does not exist or has been deleted.`));
     }
+    log(`getting metadata for hash = ${hash}`);
     const db = getDb();
     const metadata = await getMetadata(db, hash);
     ctx.body = JSON.stringify(metadata);
