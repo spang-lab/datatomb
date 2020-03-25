f92d930dc742570c88d86d792482fb385539e7fd
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2020-03-25 10:29:03 +0100

    improve error handling & logging.

--- src/middleware/isUser.js
+++ src/middleware/isUser.js
@@ -2,6 +2,7 @@ import { log } from '../util/index.js';
 
 export default async (ctx, next) => {
     if (!ctx.state.authdata.isUser) {
+        ctx.response.status = 401;
         throw (new Error('only available to registered and allowed users.'));
     }
     log(`user access for ${ctx.state.authdata.user}`);
