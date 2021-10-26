614a0538e068ce480cc6f3e0247fdc9d048542d6
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2021-10-26 15:52:37 +0200

    resolve ids that are aliases.

--- src/middleware/resolveIdentifier.js
+++ src/middleware/resolveIdentifier.js
@@ -1,5 +1,6 @@
 import {
     getDb, hashesLike,
+    aliasExists, getAlias,
 } from '../database/index.js';
 
 export default async (ctx, next) => {
@@ -14,17 +15,26 @@ export default async (ctx, next) => {
     if (id.match(reFull)) {
         ctx.params.hash = id;
     } else {
-        // no exact match
-        // find hashes that have dsetid as a substring
+        // no exact match. go through what we have
         const db = getDb(ctx);
-        const matches = await hashesLike(db, id, true);
-        if (matches.length === 0) {
-            ctx.throw(404, `no hash matches abbreviated id ${id}`);
-        } else if (matches.length > 1) {
-            ctx.throw(400, `Abbreviated id ${id} is ambiguous.`);
-        } else {
-            [ctx.params.hash] = matches;
+        //
+        // it may be a alias:
+        if( await aliasExists(db, id) ){
+            const { hash } = await getAlias(db, id);
+            ctx.params.hash = hash;
             ctx.params.abbrev = id;
+        } else {
+            // it is not a alias, find similar (abbreviated) hashes
+            // find hashes that have dsetid as a substring
+            const matches = await hashesLike(db, id, true);
+            if (matches.length === 0) {
+                ctx.throw(404, `no hash matches abbreviated id ${id}`);
+            } else if (matches.length > 1) {
+                ctx.throw(400, `Abbreviated id ${id} is ambiguous.`);
+            } else {
+                [ctx.params.hash] = matches;
+                ctx.params.abbrev = id;
+            }
         }
     }
     await next();
