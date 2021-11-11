685a6a217c8cf77fe101ef81800e9395e154622b
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2021-11-11 13:00:50 +0100

    minor adjustments

--- src/util/auth.js
+++ src/util/auth.js
@@ -4,7 +4,8 @@ import {
 } from './logger.js';
 
 const isInGroup = (userdata, groupname) => {
-    const res = userdata.groups.find((g) => (g.name === groupname));
+    log(userdata);
+    const res = userdata.grouplist.find((g) => (g === groupname));
     if (res) {
         return true;
     }
@@ -16,20 +17,21 @@ export default async (authserverconfig, authtoken) => {
         const userinfourl = `${url}/user/info`;
 
         log(`contacting authserver ${userinfourl}`);
-        const userdata = await fetch(userinfourl, {
+        const response = await fetch(userinfourl, {
             method: 'get',
             headers: {
                 Authorization: authtoken,
             },
         }).then((res) => res.json());
 
-        if (typeof userdata.ok !== 'undefined' && !userdata.ok) {
-            if (userdata.error) {
-                throw new Error(`authserver returned: ${userdata.error}`);
+        if (typeof response.ok !== 'undefined' && !response.ok) {
+            if (response.error) {
+                throw new Error(`authserver returned: ${response.error}`);
             } else {
                 throw new Error('authserver returned generic error.');
             }
         }
+        const userdata = response.data;
 
         log(`user ${userdata.sub} authenticated.`);
 
